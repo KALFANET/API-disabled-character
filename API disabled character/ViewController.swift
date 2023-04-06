70717ee0d1bcbd6ebdadee5f6e76a32a5bf852d9
KALFANET <netanel.kalfa@kalfa.me>
2023-04-06 23:56:36 +0300

    Initial Commit

---
+++ API disabled character/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  API disabled character
+//
+//  Created by Netanel Mevorach KALFA on 06/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
